930e97eb82028cffe11f6672dab84087384a7341
nindaba <arthurninda@gmail.com>
2021-12-06 20:27:37 +0100

    webflux activated in the gateway in order to register the cors in the embed server

--- gateway-service/src/main/java/com/yadlings/gateway/Security/WebSecurity.java
+++ gateway-service/src/main/java/com/yadlings/gateway/Security/WebSecurity.java
@@ -2,6 +2,8 @@ package com.yadlings.gateway.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
@@ -14,6 +16,18 @@ import java.util.Arrays;
 public class WebSecurity{
     @Value("${allowed.origins}")
     private String origins;
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity serverHttpSecurity){
+        return serverHttpSecurity
+                .csrf().disable()
+                .cors()
+                .and()
+                .authorizeExchange()
+                .anyExchange()
+                .permitAll()
+                .and()
+                .build();
+    }
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration corsConfiguration = new CorsConfiguration();
