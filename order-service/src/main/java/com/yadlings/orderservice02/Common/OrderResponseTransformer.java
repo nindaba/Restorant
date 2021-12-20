deeb19ef4aa3d2c9c9868530c78ea254cfa761c1
nindaba <arthurninda@gmail.com>
2021-12-20 09:32:29 +0100

    index on master: a0e5e2b Merge commit 'stash'

---
+++ order-service/src/main/java/com/yadlings/orderservice02/Common/OrderResponseTransformer.java
@@ -0,0 +1,20 @@
+package com.yadlings.orderservice02.Common;
+
+import com.yadlings.orderservice02.Documents.Order;
+import com.yadlings.orderservice02.Models.UpdateResponse;
+import org.springframework.http.HttpStatus;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+
+public interface OrderResponseTransformer {
+    static UpdateResponse transformUpdate(Order order){
+        System.out.println("#############################################");
+        System.out.println(order);
+        return new UpdateResponse(
+                        "Order with Id "+order.getOrderId()+" Was Updated",
+                        "UPDATE",
+                        HttpStatus.OK,
+                        LocalDateTime.now().toString());
+    }
+}
